ccfd21a4b196a6eff8c0f2560be7803b05e646ea
leonhom <leonhom@msn.cn>
2022-09-20 10:38:24 +0800

    0920更新

---
+++ src/main/java/com/leonhom/leetcode/linked/Offer22Easy408.java
@@ -0,0 +1,34 @@
+package com.leonhom.leetcode.linked;
+
+import com.leonhom.leetcode.linked.entity.ListNode;
+
+/**
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ *
+ * @408 2009年算法原题
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+ *
+ * 示例：
+ * 给定一个链表: 1->2->3->4->5, 和 k = 2.返回链表 4->5.
+ * @author: leon wang
+ * @since: 2022/09/20
+ */
+public class Offer22Easy408 {
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
